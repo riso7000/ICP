8ab72aee65d6e0bb55d2dee8818708117b6c82c8
Šimon Varga <simon.varga@outlook.sk>
2026-05-06 21:05:10 +0200

    Added JSON library, created JSON handling module, compatibility changes for VS Code.

---
+++ src/net_def_io.cpp
@@ -0,0 +1,14 @@
+#include "../third_party/nlohmann/json.hpp"
+#include <iostream>
+#include <fstream>
+
+using json = nlohmann::json;
+
+int read_ned_def(string path) {
+    std::ifstream file(path);
+    json data = json::parse(file);
+
+
+
+    return 0;
+}
