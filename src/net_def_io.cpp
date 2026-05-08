68c793e6486c4b7d2affa179e18953127f672bbd
Šimon Varga <simon.varga@outlook.sk>
2026-05-08 14:42:34 +0200

    Started file I/O implementation.

--- src/net_def_io.cpp
+++ src/net_def_io.cpp
@@ -8,38 +8,70 @@
 #include "petriscene.h"
 #include <iostream>
 #include <fstream>
+#include <QFile>
 #include <QJsonDocument>
 #include <QJsonArray>
 #include <QJsonObject>
- #include <QJsonParseError>
- #include <QJsonParseError>
+#include <QJsonParseError>
+#include <QJsonParseError>
 
-int read_netdef(std::string path) {
+int read_netdef(QString path, PetriScene& net) {
     using namespace std;
 
-    ifstream InputFile(path);
+    QString contents;
 
-    if (!InputFile) {
-        cerr << "Error: Failed to open input file." << endl;
-    }
+    QFile input_file;
+    input_file.setFileName(path);
+    input_file.open(QIODevice::ReadOnly | QIODevice::Text);
+    contents = input_file.readAll();
+    input_file.close();
 
-    InputFile.close();
+    QJsonDocument doc = QJsonDocument::fromJson(contents.toUtf8());
+    QJsonObject obj = doc.object();
+
+
 
     return 0;
 }
 
-int write_netdef(PetriScene& netdef) {
+int write_netdef(PetriScene& net) {
     using namespace std;
 
+    // Open file for writing
     ofstream OutputFile("petrinet-spec.pnet.json");
 
     if (!OutputFile) {
         cerr << "Error: Failed to open output file." << endl;
     }
 
+    // Create JSON representation
     QJsonObject json_root;
+    QJsonArray net_inputs;
+    QJsonArray net_outputs;
+    QJsonArray net_variables;
+    QJsonArray net_places;
+    QJsonArray net_transitions;
+
+    json_root.insert(JSONFLD_NET_NAME, "");
+    json_root.insert(JSONFLD_COMMENT, "");
+
+    // APPEND INPUTS
+    // APPEND OUTPUTS
+    // APPEND VARIABLES
+    
+
+    // PLACES
 
 
+    // TRANSITIONS
+
+    json_root.insert(JSONFLD_INPUTS, net_inputs);
+    json_root.insert(JSONFLD_OUTPUTS, net_outputs);
+    json_root.insert(JSONFLD_VARS, net_variables);
+    json_root.insert(JSONFLD_PLACES, net_places);
+    json_root.insert(JSONFLD_TRANSITIONS, net_transitions);
+
+    // Create JSON document from root and write it to the file
     QJsonDocument doc(json_root);
     QByteArray byte_form = doc.toJson(QJsonDocument::Indented);
 
--- src/net_def_io.hpp
+++ src/net_def_io.hpp
@@ -8,8 +8,27 @@
 #define NET_DEF_IO_HPP
 
 #include <string>
+#include "petriscene.h"
 
-int read_netdef(std::string path);
+#define JSONFLD_NET_NAME "petri_net_name"
+#define JSONFLD_COMMENT "comment"
+#define JSONFLD_INPUTS "inputs"
+#define JSONFLD_OUTPUTS "outputs"
+#define JSONFLD_VARS "variables"
+#define JSONFLD_PLACES "places"
+#define JSONFLD_TRANSITIONS "transitions"
+#define JSON_POS_X "pos_x"
+#define JSON_POS_Y "pos_y"
+#define JSON_PL_NAME "place_name"
+#define JSON_PL_INIT_TOK "initial_tokens"
+#define JSON_TR_NAME "transition_name"
+#define JSON_TR_IN "in_transition"
+#define JSON_TR_OUT "out_transition"
+#define JSON_TR_PL_TOK "tokens"
+#define JSON_TR_WHEN "when"
+#define JSON_TR_DO "do"
+
+int read_netdef(std::string path, PetriScene& net);
 int write_netdef(PetriScene& netdef);
 
 #endif
